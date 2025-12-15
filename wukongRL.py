23e43de730d2b32b65de7702e25dcf4c60530262
Code Monkey King <freesoft.for.people@gmail.com>
2025-12-15 02:42:04 +0200

    added render all routine, tile class

--- wukongRL.py
+++ wukongRL.py
@@ -1,8 +1,16 @@
 #!/bin/python3
 import curses, time, sys
 
-WIDTH = 79
-HEIGHT = 23
+SCREEN_WIDTH = 80
+SCREEN_HEIGHT = 24
+MAP_WIDTH = SCREEN_WIDTH
+MAP_HEIGHT = SCREEN_HEIGHT-1
+
+class Tile:
+  def __init__(self, blocked, block_sight=None):
+    self.blocked = blocked
+    if block_sight is None:  block_sight = blocked
+    self.block_sight = block_sight
 
 class GameObject:
   def __init__(self, x, y, ch, scr):
@@ -12,8 +20,9 @@ class GameObject:
     self.scr = scr
     
   def move(self, dx, dy):
-    self.x += dx
-    self.y += dy
+    if not map[self.x + dx][self.y + dy].blocked:
+      self.x += dx
+      self.y += dy
     
   def draw(self):
     self.scr.addch(self.y, self.x, self.ch)
@@ -21,10 +30,26 @@ class GameObject:
   def clear(self):
     self.scr.addch(self.y, self.x, ' ')
 
-def clear_screen(scr):
-  for row in range(HEIGHT):
-    for col in range(WIDTH):
-      scr.addch(row, col, ' ')
+def make_map():
+  global map
+  map = [[Tile(False) for y in range(MAP_HEIGHT)] for x in range(MAP_WIDTH)]
+  map[30][11].blocked = True
+  map[30][11].block_sight = True
+  map[40][11].blocked = True
+  map[40][11].block_sight = True
+
+def render_all(scr, objects):
+  for y in range(MAP_HEIGHT):
+    for x in range(MAP_WIDTH):
+      wall = map[x][y].block_sight
+      if wall: scr.addch(y, x, '#')
+      else: scr.addch(y, x, '.')
+  for object in objects: object.draw()
+  curses.curs_set(0)
+  player = objects[0]
+  scr.move(player.y, player.x)
+  curses.curs_set(1)
+  scr.refresh()
 
 def handle_command(scr, player):
   ch = scr.getch()
@@ -35,22 +60,20 @@ def handle_command(scr, player):
   elif ch == ord('l'): player.move(1, 0)
   
 def main(scr):
+  rows, cols = scr.getmaxyx()
+  if (rows < SCREEN_HEIGHT or cols < SCREEN_WIDTH):
+    raise RuntimeError('Set your terminal to at least 80x24')
+  scr.nodelay(1)
   curses.noecho()
-  curses.cbreak()
+  curses.raw()
   scr.keypad(1)
-  ROWS, COLS = scr.getmaxyx()
-  if (ROWS < HEIGHT or COLS < WIDTH): raise RuntimeError('Set your terminal to at least 80x24')
   curses.use_default_colors()
-  player = GameObject(WIDTH//2, HEIGHT//2, '@', scr)
-  monster = GameObject(WIDTH//2-5, HEIGHT//2, 'M', scr)
+  player = GameObject(SCREEN_WIDTH//2, SCREEN_HEIGHT//2, '@', scr)
+  monster = GameObject(SCREEN_WIDTH//2-5, SCREEN_HEIGHT//2, 'M', scr)
   objects = [player, monster]
+  make_map()
   while True:
-    curses.curs_set(0)
-    clear_screen(scr)
-    for object in objects: object.draw()
-    scr.move(player.y, player.x)
-    curses.curs_set(1)
-    scr.refresh()
+    render_all(scr, objects)
     handle_command(scr, player)
     for object in objects: object.clear()
 
